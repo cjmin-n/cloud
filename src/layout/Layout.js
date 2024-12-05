d17ff06f84dda5696f9332287ebb2c36e5f949ac
jeongmin <mymemose@naver.com>
2024-12-05 10:33:41 +0900

    first commit

---
+++ src/layout/Layout.js
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../component/Navbar";
+import Header from "../component/Header";
+
+const Layout = () => {
+    return(
+        <>
+            <Header/>
+            <Navbar/>
+            <Outlet/>
+        </>
+    );
+}
+
+export default Layout;
